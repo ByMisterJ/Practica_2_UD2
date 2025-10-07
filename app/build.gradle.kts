8121c727d96afb3f3b82cd4daf9a885bf899cbe3
josarapar <josearanda9000@gmail.com>
2025-10-07 16:29:24 +0200

    Cambios en la visualizacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.example.practica2ud2"
+    namespace = "com.example.practica1ud2"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.practica2ud2"
+        applicationId = "com.example.practica1ud2"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
@@ -29,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
@@ -37,6 +41,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
